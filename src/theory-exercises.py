af44f7ac9d9440d66b4fe8d272a241bd24882171
Jay Pottharst <sharlaon@gmail.com>
2025-04-08 16:50:34 -0400

    Add default kernel (pixi)

--- src/theory-exercises.py
+++ src/theory-exercises.py
@@ -8,6 +8,10 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: default
+#     language: python
+#     name: python3
 # ---
 
 # %% [markdown]
